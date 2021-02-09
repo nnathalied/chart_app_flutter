01aa6e96daa218323e0079cd6439deea20ea15ef
Noelle <nndea@ucdavis.edu>
2021-02-09 00:46:53 -0800

    trying to add scatter chart

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:chart_app_flutter/src/chart1.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text("Hello, World! This is my homepage!"),
+        child: SimpleScatterPlotChart(),
       ),
     );
   }
