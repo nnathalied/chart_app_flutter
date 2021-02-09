3f77411f21e96f583b22a628e32f2e92e1be228b
Noelle <nndea@ucdavis.edu>
2021-02-09 13:08:55 -0800

    time to mess around with the sample

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:chart_app_flutter/home.dart';
 import 'package:flutter/material.dart';
+import 'package:chart_app_flutter/src/home.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
+    return MaterialApp(home: HomePage());
   }
 }
-
-// import 'package:flutter/material.dart';
-// import 'package:chart_app_flutter/src/home.dart';
-
-// void main() => runApp(MyApp());
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(home: HomePage());
-//   }
-// }
